5e4622fd3407d71984a89771e9ae02ce0dfea128
Hendri Sulistya <id.hendrisulistya@gmail.com>
2022-10-19 18:25:48 +0700

    body description

--- src/components/body.js
+++ src/components/body.js
@@ -5,9 +5,7 @@ function body() {
     <div className='container-fluid bg-black text-white'>
         <div className='row d-flex align-items-center justify-content-center' style={{height:1000}}>
             <div className='col-12 mt-5 mb-5 text-center'>
-                <h1>We fund research and development teams</h1>
-                <h1> who are building the foundation of the</h1>
-                <h1>decentralized web. Join us.</h1>
+                <h1>The story begins with several people who are motivated in the development of blockchain technology, but on the one hand are trapped in a complicated love affair. We are determined to facilitate a return to our passion for technology</h1>
             </div>
             <div className='row text-center mt-5 mb-5'>
                 <div className='col-3'>ATOM</div>
